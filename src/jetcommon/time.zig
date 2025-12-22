6916e0e955da5016c64eab83c3cd80461db964bf
Ben Jordan <ben@poecoh.com>
2025-12-21 19:25:30 -0500

    pointing format directly to zul

--- src/jetcommon/time.zig
+++ src/jetcommon/time.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
+const zul = @import("zul");
+
 pub const DateTime = @import("time/DateTime.zig");
 pub const Date = @import("time/Date.zig");
 pub const Time = @import("time/Time.zig");
-pub const TimestampPrecision = enum { seconds, milliseconds, microseconds };
+pub const TimestampPrecision = zul.DateTime.TimestampPrecision;
--- src/jetcommon/time/Time.zig
+++ src/jetcommon/time/Time.zig
@@ -1,22 +1,16 @@
 const std = @import("std");
 
 const zul = @import("zul");
+pub const Format = zul.Time.Format;
 
 zul_time: zul.Time,
 
 const Time = @This();
 
-pub const Format = enum {
-    rfc3339,
-};
-
 pub fn init(hour: u8, min: u8, sec: u8, micros: u32) !Time {
-    const zul_time = try zul.Time.init(hour, min, sec, micros);
-    return .{ .zul_time = zul_time };
+    return .{ .zul_time = try .init(hour, min, sec, micros) };
 }
 
 pub fn parse(input: []const u8, fmt: Format) !Time {
-    return switch (fmt) {
-        .rfc3339 => .{ .zul_time = try zul.Time.parse(input, .rfc3339) },
-    };
+    return .{ .zul_time = try .parse(input, fmt) };
 }
